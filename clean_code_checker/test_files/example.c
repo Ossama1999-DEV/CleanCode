faecf31925ec6d1c5f3fa04f3c27d5ac41b38b38
DBIBIH Oussama <oussama.dbibih@viacesi.fr>
2025-02-05 22:29:54 +0100

    Add additional test cases to example.c for function naming and macro usage violations

--- clean_code_checker/test_files/example.c
+++ clean_code_checker/test_files/example.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
-int main() {
+// Nom de fonction non descriptif et ne respectant pas le camelCase
+void myfonctioncesdunimportequoi() { 
     printf("Hello, World!\n");
+}
+
+// Mauvaise utilisation des macros (nom non explicite)
+#define oussama dbibih smiti 
+
+int main() {
+    // Variable avec un nom très long et peu compréhensible
+    int helosalamao3alaykjomhbabio = 0;
+
+    printf("Hello, World!\n");
+
     return 0;
 }
